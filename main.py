74443e5fc214da365f086066a43bd247667cdb87
ThisProgrammerG <116992225+ThisProgrammerG@users.noreply.github.com>
2022-12-27 08:27:22 -0600

    Refactor and add gitignore.

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 import os
+from os import path
 import argparse
 from itertools import filterfalse, tee
+from typing import Iterable, Callable
 
 SEP = '/'
+TEE = '├── '
+CORNER = '└── '
+SPACER = '│   '
 
-def partition(func, iterable):
+def partition(func: Callable, iterable: Iterable) -> tuple[Iterable, Iterable]:
     first, second = tee(iterable, 2)
     return filterfalse(func, first), filter(func, second)
 
-def filter_ignores(files, folders):
-    out_file_ignore = lambda file: file.find('.') != 0
-    out_folder_ignore = lambda folder: folder.find('pycache') < 0 and folder.find('venv') < 0 and folder.find('.') < 0
-    return tuple(filter(out_file_ignore, files)), tuple(filter(out_folder_ignore, folders))
+def file_ignores(file: str) -> bool:
+    return file.find('.') != 0
 
-def _print_files(files, indent):
+def folder_ignores(folder: str) -> bool:
+    return folder.find('pycache') < 0 and folder.find('venv') < 0 and folder.find('.') < 0
+
+def filter_ignores(files: Iterable, folders: Iterable) -> tuple[tuple, tuple]:
+    return tuple(filter(file_ignores, files)), tuple(filter(folder_ignores, folders))
+
+def _print_files(files: tuple, indent: str) -> None:
     for file in files:
-        if file == files[-1]:
-            print(f'{indent}└── {file}')
-        else:
-            print(f'{indent}├── {file}')
+        shape = CORNER if file == files[-1] else TEE
+        print(f'{indent}{shape}{file}')
 
-def _print_folders(root_directory, folders, indent):
+def _print_folders(root_directory: str, folders: tuple, indent: str) -> None:
     for folder in folders:
-        if folder == folders[-1]:
-            print(f'{indent}└── {folder}{SEP}')
-        else:
-            print(f'{indent}├── {folder}{SEP}')
+        shape = CORNER if folder == folders[-1] else TEE
+        print(f'{indent}{shape}{folder}{SEP}')
 
         next_directory_path = os.path.join(root_directory, folder)
-        print_directory_tree(next_directory_path, f'{indent}│   ')
+        print_directory_tree(next_directory_path, f'{indent}{SPACER}')
 
-def print_directory_tree(root_directory, indent=''):
+def print_directory_tree(root_directory: path, indent: str = '') -> None:
     contents = os.listdir(root_directory)
     files, folders = partition(lambda item: os.path.isdir(os.path.join(root_directory, item)), contents)
     files, folders = filter_ignores(files, folders)
@@ -38,7 +43,7 @@ def print_directory_tree(root_directory, indent=''):
     _print_files(files, indent)
     _print_folders(root_directory, folders, indent)
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help='the directory path to process', nargs="?", action='store')
     args = parser.parse_args()
